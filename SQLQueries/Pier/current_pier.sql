bb0efd38d473b7d5b940db18c192910c9c111f92
Unknown <stephen.costanzo@snhu.edu>
2018-10-29 16:27:42 -0700

    UI-471 and other changes

--- SQLQueries/Pier/current_pier.sql
+++ SQLQueries/Pier/current_pier.sql
@@ -19,7 +19,7 @@ SELECT 'PIER' Application,
 	''												approval, 
 	''												approval_history, 
 	-- VP value 
-	usr.full_name								         Assignee, 
+	usr.full_name								Assignee, 
 	grp.group_name								Assignee_Group, 
 	''												number, 
 	tt.created_date							Task_Opened_Date, 
@@ -32,7 +32,7 @@ SELECT 'PIER' Application,
 	ad2.fullname Closed_By, 
 	tt.ticket_sla								SLA,
 	CASE 
-		WHEN tt.status IN ( 2, 4, 8 )	
+		WHEN sts.Is_Open = 0 --tt.status IN ( 2, 4, 8 )	
 			THEN Datediff(mi, tt.created_date, 
 			CASE 
 				WHEN tt.resolved_date IS NULL 
@@ -45,7 +45,9 @@ SELECT 'PIER' Application,
 		Row_number() 
 			OVER( 
 				partition BY tt.created_date 
-			ORDER BY tt.created_date DESC) AS Row 
+			ORDER BY tt.created_date DESC) AS Row ,
+	CONCAT('https://technology.services.t-mobile.com/pier#Ticket?ticket_id=', tt.tt_id) as TicketURL,
+	tt.priority_id								PriorityLevel
 FROM BI_Semantics.rpt_tm.v_trouble_ticket tt WITH (nolock) 
 		INNER JOIN BI_Semantics.usergroup.v_gu_group grp WITH (nolock) 
 			ON tt.assignee_group = grp.group_id 
@@ -65,4 +67,4 @@ FROM BI_Semantics.rpt_tm.v_trouble_ticket tt WITH (nolock)
 			ON ad2.loginname = tt.closed_by 
 WHERE grp.group_name IN ( 'EIT Inf Ops Support UNIX Tier 2' ) 
 		AND tt.created_date >= '1/1/2017' 
-		and sym.Symptom_Desc = 'Degraded Service'
+		--and sym.Symptom_Desc = 'Degraded Service'
