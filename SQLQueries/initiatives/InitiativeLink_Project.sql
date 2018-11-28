4c892761ac8b19d5291cb1a381e2388266be5222
Unknown <stephen.costanzo@snhu.edu>
2018-11-28 15:55:23 -0800

    Latest Updates inc. initiatives

--- SQLQueries/initiatives/InitiativeLink_Project.sql
+++ SQLQueries/initiatives/InitiativeLink_Project.sql
@@ -6,6 +6,7 @@ SELECT DISTINCT  DJI_Init.jira_issue_dwkey 'Initiative DWKey'
 	, DJI.summary 'Epic Summary'
 	, DJP.jira_proj_dwkey 'Project DWKey'
 	, DJP.jira_proj_key_cd 'Project'
+	, COMP.component_name 'Component Name'
 FROM dim_jira_issue DJI_Init
 INNER JOIN fact_jira_entity_property FJEP
 	ON FJEP.jira_parent_issue_dwkey= DJI_Init.jira_issue_dwkey
@@ -17,5 +18,12 @@ INNER JOIN dim_jira_proj DJP
 	ON djp.jira_proj_dwkey = fji.jira_proj_dwkey
 INNER JOIN dim_jira_issue DJI
 	ON DJI.jira_issue_dwkey = FJI.jira_issue_dwkey
+LEFT JOIN (
+		SELECT jira_issue_dwkey, component_name
+		FROM fact_jira_issue_component FJIC
+		INNER JOIN dim_jira_component DJC 
+		ON FJIC.jira_component_dwkey = DJC.jira_component_dwkey
+	) COMP
+	ON DJI_Init.jira_issue_dwkey = COMP.jira_issue_dwkey
 WHERE djit.issue_type = 'Epic'
-	AND djp.jira_proj_key_cd IN ('INFAOP', 'INFUOP', 'CF', 'NAS', 'STOR')
+	AND djp.jira_proj_key_cd IN ('INFAOP', 'INFUOP',  'NAS', 'STOR')
